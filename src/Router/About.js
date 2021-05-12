a0d46334b498de41624cbc99d43b952fc96c0787
nitindevops21 <83716720+nitindevops21@users.noreply.github.com>
2021-05-12 20:11:23 +0530

    Committing AWS

---
+++ src/Router/About.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            
+            <h1>developed by Kunal and Avirup</h1>
+         );
+    }
+}
+ 
+export default About;
